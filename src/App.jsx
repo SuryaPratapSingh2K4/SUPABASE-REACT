723e1b3c49a327325a9fdda4236489063b29a16d
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-09-17 01:24:58 +0530

    add and fetch data from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import { supabase } from './supabaseClient'
+// import { SupabaseClient } from '@supabase/supabase-js';
+import { supabase } from './supabase-client';
 
 function App() {
-  const [task, setTask] = useState('');
+  const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [email, setEmail] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    const { data, error } = await supabase.from("tasks").select("*").order("created_at", { ascending: true });
+    if (error) {
+      console.error("Fetch Error:", error);
+      alert("Fetch failed: " + error.message);
+      return;
+    }
+    console.log(data);
+    setTasks(data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
   const handleAddStudent = async (e) => {
     e.preventDefault();
-    await supabase.from("tasks")
+    const { data, error } = await supabase.from("tasks").insert([{ title, description, email }]);
+
+    if (error) {
+      console.error("Insert error:", error);
+      alert("Insert failed: " + error.message);
+      return;
+    }
+    console.log("Insert successful:", data);
+    alert("Task added successfully!");
+
+    setTitle("");
+    setDescription("");
+    setEmail("");
+
   }
 
   return (
     <div className='flex flex-col'>
       <h1 className="text-2xl font-bold text-center mt-10">Add Task Details</h1>
       <div className="flex flex-col mt-4 p-4 w-3/4 md:w-1/2 mx-auto">
-        <label className="mb-2 font-bold">Task:</label>
-        <input type="text" placeholder='Enter task' value={task} onChange={(e) => setTask(e.target.value)} className="border rounded-lg border-gray-400 p-2 mb-4" />
+        <label className="mb-2 font-bold">Title:</label>
+        <input type="text" placeholder='Enter title' value={title} onChange={(e) => setTitle(e.target.value)} className="border rounded-lg border-gray-400 p-2 mb-4" />
 
         <label className="mb-2 font-bold">Task Description:</label>
         <input type="text" placeholder='Enter Description' value={description} onChange={(e) => setDescription(e.target.value)} className="border rounded-lg border-gray-400 p-2 mb-4" />
@@ -31,7 +61,24 @@ function App() {
         <button onClick={handleAddStudent} className="bg-blue-500 text-white p-2 rounded-lg">Add Student</button>
       </div>
 
-      {}
+      {tasks && tasks.length > 0 ?
+        <div>
+          <h2 className="text-xl font-bold text-center mt-10">Task List</h2>
+          <div>
+            {
+              tasks.map((t) => (
+                <div key={t.id} className='border p-4 m-4 rounded-lg shadow-md'>
+                  <h3 className='font-bold'>{t.title}</h3>
+                  <p>{t.description}</p>
+                  <p className='text-gray-600'>{t.email}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div> : <div>
+          <h2 className="text-xl font-bold text-center mt-10">No Tasks Available</h2>
+        </div>
+      }
 
     </div>
 
